b45ace1ccd2ad620d061f0153b08ca44a1830c92
Sun Hao <wshadowhunter@yahoo.cn>
2011-10-07 09:19:21 -0400

    se

--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,6 @@ ActiveRecord::Schema.define(:version => 20111004045858) do
   create_table "products", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "image_url"
-    t.string   "name"
-    t.integer  "vote_num"
-    t.integer  "uid"
-    t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -50,7 +45,6 @@ ActiveRecord::Schema.define(:version => 20111004045858) do
   create_table "reply_topics", :force => true do |t|
     t.integer  "uid"
     t.integer  "pid"
-    t.string   "name"
     t.string   "title"
     t.integer  "vote_num"
     t.text     "content"
@@ -62,7 +56,6 @@ ActiveRecord::Schema.define(:version => 20111004045858) do
     t.string   "name"
     t.string   "hashed_password"
     t.string   "salt"
-    t.integer  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -70,7 +63,6 @@ ActiveRecord::Schema.define(:version => 20111004045858) do
   create_table "votes", :force => true do |t|
     t.integer  "uid"
     t.integer  "pid"
-    t.integer  "sub_level"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
